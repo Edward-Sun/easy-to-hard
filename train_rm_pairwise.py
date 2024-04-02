4e32f017b571a4f78cd39aa689ab1ac25ae21c72
Edward-Sun <zhiqings@cs.cmu.edu>
2024-04-02 19:05:38 -0400

    fix the wrong uploaded file of rm_pairwise

--- train_rm_pairwise.py
+++ train_rm_pairwise.py
@@ -10,9 +10,11 @@ import sys
 import tempfile
 import time
 from pathlib import Path
+from typing import Dict
 import itertools
 
 import tqdm
+import einops
 
 import torch
 import torch.nn.functional as F
@@ -50,8 +52,8 @@ from models.tp import (
 )
 
 from trainers.common_utils import manual_seed
-from data_utils.data_utils_rm_pointwise import (
-    make_pointwise_reward_modeling_data_module,
+from data_utils.data_utils_rm_pairwise import (
+    make_pairwise_reward_modeling_data_module,
 )
 
 from hf_argparser import HfArgumentParser
@@ -71,44 +73,78 @@ from training_utils.trainer_utils import (
 IGNORE_INDEX = -100
 
 
-def model_forward(model, x):
-    return model(x)
+def model_forward(model, x, eos_pos):
+    return model(x, eos_pos)
 
 
 def model_forward_with_loss(
     model: RewardModel,
     input_ids: torch.Tensor,
-    labels: torch.Tensor,
-    weights: torch.Tensor,
+    choice: torch.Tensor,
 ) -> torch.Tensor:
     """
     Compute the loss for a given model and prompts.
     """
     # create an empty tensor of the expected final shape and fill in the current tokens
-    batch_size, T = input_ids.size(0), input_ids.size(1)
+    # input_ids: (bsz, num_candidates, max_seq_len)
+    # choice: (bsz, num_pairs)
+    batch_size, num_candidates, T = (
+        input_ids.size(0),
+        input_ids.size(1),
+        input_ids.size(2),
+    )
+
+    assert choice.size(0) == batch_size
+    assert choice.size(1) == 1
+    assert num_candidates == 2
 
     device = input_ids.device
     with torch.device(device):
         model.backbone_model.setup_caches(
-            max_batch_size=batch_size, max_seq_length=T, kv_cache=False
+            max_batch_size=batch_size * 2, max_seq_length=T, kv_cache=False
         )
+
+    input_ids = einops.rearrange(input_ids, "b c t -> (b c) t")
+    eos_pos = input_ids.ne(0).long().sum(dim=-1) - 1
 
     with torch.backends.cuda.sdp_kernel(
         enable_flash=True, enable_math=False, enable_mem_efficient=False
     ):
-        logits = model_forward(model, input_ids)
+        rewards = model_forward(model, input_ids, eos_pos)
+
+    rewards = einops.rearrange(rewards, "(b c) -> b c", b=batch_size, c=num_candidates)
+    logits = rewards[:, 0] - rewards[:, 1]
 
     with torch.autocast(device_type="cuda", enabled=False):
         dtype_logits = logits.float()
         loss = F.binary_cross_entropy_with_logits(
             dtype_logits.view(-1).float(),
-            labels.view(-1).float(),
-            reduction="none",
+            choice.view(-1).float(),
+            reduction="mean",
         )
 
-        weights = weights.view(-1).float()
-        loss = (loss * weights).sum() / (weights.sum() + 1e-6)
-    return loss
+        metrics = compute_reward_modeling_metrics(logits.view(-1), choice.view(-1))
+
+    return loss, metrics
+
+
+def compute_reward_modeling_metrics(logits, labels) -> Dict:
+    # eval_prediction.label_ids is a tuple that matches up with `training_args.label_names`.
+    predictions = (logits >= 0.0).long()
+    accuracy = predictions.eq(labels).float().mean()
+    label_positive_rate = (labels == 1).float().mean()
+    positive_rate = (predictions == 1).float().mean()
+    true_positive_rate = (predictions * labels).float().sum() / labels.sum()
+    false_positive_rate = (predictions * (1 - labels)).float().sum() / (
+        1 - labels
+    ).sum()
+    return dict(
+        accuracy=accuracy,
+        label_positive_rate=label_positive_rate,
+        positive_rate=positive_rate,
+        true_positive_rate=true_positive_rate,
+        false_positive_rate=false_positive_rate,
+    )
 
 
 def encode_tokens(tokenizer, string, bos=True, device="cuda"):
@@ -131,10 +167,7 @@ def main(
         assert sft_checkpoint_path.is_dir(), sft_checkpoint_path
 
     tokenizer_path = checkpoint_path.parent / "tokenizer.model"
-    if not tokenizer_path.is_file():
-        tokenizer_path = checkpoint_path.parent
-
-    set_global_compile_mode(args.compile)
+    assert tokenizer_path.is_file(), tokenizer_path
 
     global print
     device_id = maybe_init_dist()
@@ -195,7 +228,7 @@ def main(
         precision,
         use_tp,
         requires_grad=True,
-        sequence_parallel=args.sequence_parallel,
+        reward_head_init_scheme=args.reward_head_init_scheme,
     )
 
     torch.cuda.synchronize()
@@ -205,7 +238,7 @@ def main(
 
     tokenizer = AcceleraTokenizer(tokenizer_path)
 
-    data_module = make_pointwise_reward_modeling_data_module(
+    data_module = make_pairwise_reward_modeling_data_module(
         tokenizer=tokenizer,
         args=args,
     )
@@ -242,14 +275,22 @@ def main(
 
     if args.print_training_examples:
         print("Training examples:")
-        cnt = 16
+        cnt = 3
         for batch in train_loader:
+            print("Input 1:")
             print(
-                "Input:",
                 tokenizer.decode(
-                    batch["input_ids"][0].tolist(), skip_special_tokens=False
+                    batch["input_ids"][0, 0].tolist(), skip_special_tokens=False
                 ),
             )
+            print("=" * 20)
+            print("Input 2:")
+            print(
+                tokenizer.decode(
+                    batch["input_ids"][0, 1].tolist(), skip_special_tokens=False
+                ),
+            )
+            print("=" * 40)
             cnt -= 1
             if cnt == 0:
                 break
@@ -266,6 +307,7 @@ def main(
     if get_data_parallel_world_size() > 1:
         use_fsdp = True
         model = create_fsdp_model_for_finetune(args, model)
+
         print("Using FSDP ...")
         print(model)
 
@@ -336,13 +378,11 @@ def main(
                 model.zero_grad()
 
                 input_ids = batch["input_ids"].to(device=device)
-                labels = batch["labels"].to(device=device)
-                weights = batch["weights"].to(device=device)
+                choice = batch["choice"].to(device=device)
 
-                input_ids, labels, weights = prepare_batch(
+                input_ids, choice = prepare_batch(
                     input_ids,
-                    labels,
-                    weights,
+                    choice,
                     tokenizer=tokenizer,
                     use_tp=use_tp,
                     sync_group=tp_group,
@@ -352,55 +392,57 @@ def main(
                 for ex_idx in range(0, input_ids.size(0), micro_train_batch_size):
                     if ex_idx + micro_train_batch_size < input_ids.size(0):
                         with torch.cuda.amp.autocast(dtype=args.compute_dtype):
-                            loss = model_forward_with_loss(
+                            loss, metrics = model_forward_with_loss(
                                 model,
                                 input_ids[ex_idx : ex_idx + micro_train_batch_size],
-                                labels[ex_idx : ex_idx + micro_train_batch_size],
-                                weights[ex_idx : ex_idx + micro_train_batch_size],
+                                choice[ex_idx : ex_idx + micro_train_batch_size],
                             )
                         (loss_scale * loss).backward()
                     else:
                         with torch.cuda.amp.autocast(dtype=args.compute_dtype):
-                            loss = model_forward_with_loss(
+                            loss, metrics = model_forward_with_loss(
                                 model,
                                 input_ids[ex_idx:],
-                                labels[ex_idx:],
-                                weights[ex_idx:],
+                                choice[ex_idx:],
                             )
                         (loss_scale * loss).backward()
-                        grad_norm = clip_grad_norm_(model, 1.0)
+                        grad_norm = clip_grad_norm_(model, 5.0)
                         optimizer.step()
                         scheduler.step()
 
-                if it % 5 == 0:
-                    loss_copy = loss.detach().clone()
-                    torch.distributed.all_reduce(loss_copy)
-                    avg_loss = (loss_copy / torch.distributed.get_world_size()).item()
-                    grad_norm_copy = grad_norm.detach().clone().item()
+                loss_copy = loss.detach().clone()
+                acc_copy = metrics["accuracy"].detach().clone()
+                torch.distributed.all_reduce(loss_copy)
+                torch.distributed.all_reduce(acc_copy)
+                avg_loss = (loss_copy / torch.distributed.get_world_size()).item()
+                avg_acc = (acc_copy / torch.distributed.get_world_size()).item()
+                grad_norm_copy = grad_norm.detach().clone().item()
 
-                    if device_id == 0:
-                        if args.report_to == "wandb" and wandb is not None:
-                            wandb.log(
-                                {
-                                    "loss": avg_loss,
-                                    "learning_rate": scheduler.get_last_lr()[0],
-                                    "epoch": epoch,
-                                    "step": it,
-                                    "grad_norm": grad_norm_copy,
-                                },
-                                step=global_step,
-                            )
-                        else:
-                            # Just print to stdout.
-                            print(
-                                {
-                                    "loss": avg_loss,
-                                    "learning_rate": scheduler.get_last_lr()[0],
-                                    "epoch": epoch,
-                                    "step": it,
-                                    "grad_norm": grad_norm_copy,
-                                }
-                            )
+                if device_id == 0:
+                    if args.report_to == "wandb" and wandb is not None:
+                        wandb.log(
+                            {
+                                "loss": avg_loss,
+                                "accuracy": avg_acc,
+                                "learning_rate": scheduler.get_last_lr()[0],
+                                "epoch": epoch,
+                                "step": it,
+                                "grad_norm": grad_norm_copy,
+                            },
+                            step=global_step,
+                        )
+                    else:
+                        # Just print to stdout.
+                        print(
+                            {
+                                "loss": avg_loss,
+                                "accuracy": avg_acc,
+                                "learning_rate": scheduler.get_last_lr()[0],
+                                "epoch": epoch,
+                                "step": it,
+                                "grad_norm": grad_norm_copy,
+                            }
+                        )
 
                 checkpoint_hook(
                     args,
@@ -433,29 +475,25 @@ def main(
         print(f"Time to train: {time.time() - t0:.02f} seconds")
 
 
-def prepare_batch(input_ids, labels, weights, tokenizer, use_tp, sync_group):
+def prepare_batch(input_ids, choice, tokenizer, use_tp, sync_group):
     pad_id = tokenizer.pad_id
     unk_id = tokenizer.unk_id
-    # if pad_id < 0, replace pad_id with unk_id
-    labels[labels == pad_id] = IGNORE_INDEX
     if pad_id < 0:
         input_ids[input_ids == pad_id] = unk_id
 
     if use_tp and get_model_parallel_world_size() > 1:
         # aggregate (concat) all the inputs across tp sync_group
         new_input_ids = torch.empty_like(input_ids).repeat(sync_group.size(), 1)
-        new_labels = torch.empty_like(labels).repeat(sync_group.size(), 1)
-        new_weights = torch.empty_like(weights).repeat(sync_group.size(), 1)
+        new_choice = torch.empty_like(new_choice).repeat(sync_group.size(), 1)
 
         torch.distributed.all_gather_into_tensor(
             new_input_ids, input_ids, group=sync_group
         )
-        torch.distributed.all_gather_into_tensor(new_labels, labels, group=sync_group)
-        torch.distributed.all_gather_into_tensor(new_weights, weights, group=sync_group)
+        torch.distributed.all_gather_into_tensor(new_choice, choice, group=sync_group)
 
-        return new_input_ids, new_labels, new_weights
+        return new_input_ids, new_choice
 
-    return input_ids, labels, weights
+    return input_ids, choice
 
 
 if __name__ == "__main__":
